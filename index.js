673d2154ec9b84cd1eadda5aec99e9cb6954f065
pratyush3112 <pratyushkumar3m@gmail.com>
2025-07-21 12:04:32 +0530

    1

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-var a = [10, 20, "hello", true];
-console.log(a);
-a.push("add", "remove", 10);
-console.log(a);
+let n = 5;
+
+for (let i = 1; i <= n; i++) {
+    let line = "";
+
+    for (let s = 1; i <= n - i; s++) {
+        line += "";
+    }
+        for (let j = i; j >= 1; j--) {
+            line += j + "";
+        }
+            for (let j = 2; j <= i; j++) {
+                line += j + "";
+            }
+
+}
+console.log(line);
--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
 let a=[10,20,"hello",true];
 console.log(a);
 a.push("add","remove",10);
-console.log(a);
+console.log(a);                                                                                             
